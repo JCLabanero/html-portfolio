400e10db0d9e4376306913207cb0ca4d506ac7a3
John Carlo Labanero <johncarlolabanero@gmail.com>
2024-05-10 17:52:01 +0800

    add env file

--- express/index16-cookies-session.js
+++ express/index16-cookies-session.js
@@ -2,39 +2,43 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
-import session from "express-session"
-import passport from "passport"
+import session from "express-session";
+import passport from "passport";
 import { Strategy } from "passport-local";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
 
+env.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("static"));
 
-app.use(session({
-  secret: "THISISASECRET",//KEY to sign the session cookie
-  resave: false,//Forces the session to be saved back to the session store
-  //Even if server reset, update server and still would work
-  saveUninitialized: true,//Whether we want to force a session to be save to store
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24, //One day length cookie
-  },
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, //KEY to sign the session cookie
+    resave: false, //Forces the session to be saved back to the session store
+    //Even if server reset, update server and still would work
+    saveUninitialized: true, //Whether we want to force a session to be save to store
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, //One day length cookie
+    },
+  })
+);
 
 //TODO: SHOULD USE SESSION FIRST BEFORE THIS ONE 96
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-//TODO: STRATEGY LINE 
+//TODO: STRATEGY LINE
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "Password1!",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 db.connect();
@@ -43,22 +47,24 @@ app.get("/", (req, res) => {
   res.render("cookies-session-home.ejs");
 });
 
-app.get("/secrets",(req,res) => {
+app.get("/secrets", (req, res) => {
   console.log(req.user);
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     res.render("cookies-session-secrets.ejs");
-  } else
-    res.redirect("/login")
+  } else res.redirect("/login");
 });
 
 app.get("/login", async (req, res) => {
   res.render("cookies-session-secrets.ejs");
 });
 
-app.post("/login", passport.authenticate("local",{
-  successRedirect:"/secrets",
-  failureRedirect:"/login"
-}));
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("cookies-session-register.ejs");
@@ -100,41 +106,43 @@ app.post("/register", async (req, res) => {
   }
 });
 
-passport.use(new Strategy(async function verify(username,password,cb){
-  console.log(`${username}, ${password}`);
-  try {
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      username,
-    ]);
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-      const storedHashedPassword = user.password;
-      bcrypt.compare(password, storedHashedPassword, (err, result) => {
-        if (err) {
-          return cb(err);
-        } else {
-          if (result) {
-            res.render("secrets.ejs");
-            return cb(null, user);
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    console.log(`${username}, ${password}`);
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, result) => {
+          if (err) {
+            return cb(err);
           } else {
-            return cb(null,false);
+            if (result) {
+              res.render("secrets.ejs");
+              return cb(null, user);
+            } else {
+              return cb(null, false);
+            }
           }
-        }
-      });
-    } else {
-      return cb("User not found");
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      return cb(err);
     }
-  } catch (err) {
-    return cb(err);
-  }
-}));
+  })
+);
 
-passport.serializeUser((user, cb)=>{
-  cb(null,user);
+passport.serializeUser((user, cb) => {
+  cb(null, user);
 });
 
-passport.deserializeUser((user, cb)=>{
-  cb(null,user);
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 app.listen(port, () => {
